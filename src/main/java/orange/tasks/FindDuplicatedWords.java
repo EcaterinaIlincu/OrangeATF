5d45d197e3b1d81179d948e4d5dbafd84d55afe4
Ecaterina Ilincu <ilincue@gmail.com>
2024-03-21 22:10:03 +0200

    Added logic for finding duplicates

--- src/main/java/orange/tasks/FindDuplicatedWords.java
+++ src/main/java/orange/tasks/FindDuplicatedWords.java
@@ -9,18 +9,29 @@ public class FindDuplicatedWords {
 
     public void getAllDuplicates(String inputText) throws NoDuplicatesException {
 
+        //Initialise variable to store remaining text after first sentence is ignored
         String remainingText = "";
-        Map<String, Integer> wordMap = new HashMap<>();
-        Map<String, Integer> wordMapUnique = new HashMap<>();
 
+        // create a map to store all words
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        // create another map to store duplicated words
+        Map<String, Integer> duplicateWordMap = new HashMap<>();
+
+        // find first dot to identify first sentence
         int firstDot = inputText.indexOf('.');
 
+        // delete first sentence and store it in remainingText, remove characters:,. and lowercase all letters
         if (firstDot != -1) {
-            remainingText = inputText.substring(firstDot + 1).trim().replaceAll("[,.]", "").toLowerCase();  // extract the substring after first dot
+            remainingText = inputText.substring(firstDot + 1).trim().replaceAll("[,.]", "").toLowerCase();
         }
 
+        // splitting words from remaining text to array
         String[] words = remainingText.split(" ");
 
+
+
+      // Iterate through each word from Array String, add all words to wordMap and number of time it is encountered
         for (String word : words) {
             if (wordMap.containsKey(word)) {
                 int count = wordMap.get(word);
@@ -30,16 +41,18 @@ public class FindDuplicatedWords {
             }
         }
 
+        // iterate through each word from wordMap and store the words that are found more than once in duplicateWordMap
         for (String word : wordMap.keySet()) {
             if (wordMap.get(word) > 1) {
-                wordMapUnique.put(word, wordMap.get(word));
+                duplicateWordMap.put(word, wordMap.get(word));
             }
         }
-        for (String word : wordMapUnique.keySet()) {
-            System.out.println("Word:" + "'" + word + "'" + " is found: " + wordMapUnique.get(word) + " times");
+        // iterate through each word from duplicateWordMap and print each duplicated word and number of repeats
+        for (String word : duplicateWordMap.keySet()) {
+            System.out.println("Word:" + "'" + word + "'" + " is found: " + duplicateWordMap.get(word) + " times");
         }
-
-        if (wordMapUnique.isEmpty()) {
+        // throw exception if no duplicated words are found
+        if (duplicateWordMap.isEmpty()) {
             throw new NoDuplicatesException("No Duplicates Found");
         }
     }
